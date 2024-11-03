39a93b58f85d089a202129ca6dba6226c6343cba
Vikas Thulasi <vikasvicky372@gmail.com>
2024-11-03 23:06:09 +0530

    create react app and tailwind integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" p-4 my-10 text-3xl w-6/12 m-auto text-center font-bold text-green-900">
+      Hi People
     </div>
   );
 }
